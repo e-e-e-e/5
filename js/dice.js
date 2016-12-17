ce05065cbfe849a4d0f6b02be825657b201c18ed
Herdnaxy Hitni <herdnaxy@gmail.com>
2016-12-17 17:27:26 +0000

    Create dice.js

---
+++ js/dice.js
@@ -0,0 +1,36 @@
+function Dice() // Prepares the dice for rolling
+this.dicespread = 1;
+this.rolled = 0;
+
+}
+
+// Rolling the dice
+Dice.prototype.actuate function self.dice {
+  var Math.random() < 0.166 ? 0 : 1 : 2 : 5 : -1 : 1/1;
+  this.rolled = value;
+    
+  }
+  
+
+// Determines what happens when each symbol is rolled
+if (this.rolled = 0); {
+    var this.playervalue = currentvalue + 0; }
+    
+if (this.rolled = 1); {
+    var this.playervalue = currentvalue + 1; }
+  
+if (this.rolled = 2); {
+    var this.playervalue = currentvalue + 2; }
+    
+if (this.rolled = 5); {
+    var this.playervalue = currentvalue + 5; }
+    
+if (this.rolled = -1); {
+    var this.playervalue = currentvalue - 1; }
+    
+if (this.rolled = 1/1); {
+    var this.playervalue = 0; }
+    
+    
+{
+}
